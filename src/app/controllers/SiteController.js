8c163371e8910c980a6af80302e62ebd4641ce94
SonPhamThanh175 <90856396+SonPhamThanh175@users.noreply.github.com>
2023-12-09 03:05:59 +0700

    Update SiteController.js

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,32 @@
+const Course = require('../models/Course');
+
+const {muntipleMongooseToObject} = require('../../util/mongoose');
 class SiteController {
-    //[GET] /Home
-    index(req, res) {
-        res.render('home');
-    }
-    //[GET] /search
-    search(req, res) {
-        res.render('search');
-    }
+  // [GET] /news
+  // index(req, res, next) {
+  //   Course.find({})
+  //     .lean()
+  //     .then((courses) => {
+  //       res.render("home", { courses: muntipleMongooseToObject(courses) });
+  //     })
+  //     .catch((error) => {
+  //       next(error);
+  //     });
+  // }
+  index(req, res, next) {
+    Course.find({})
+      .lean()
+      .then((courses) => {
+        res.render("home", { courses });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+  // [GET] /search
+  search(req, res) {
+    res.render("search");
+  }
 }
 
 module.exports = new SiteController();
