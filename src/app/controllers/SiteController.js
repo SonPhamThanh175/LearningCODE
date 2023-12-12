3e44fdf90a328802499ac90d2c00881d42c8dae4
SonPhamThanh175 <90856396+SonPhamThanh175@users.noreply.github.com>
2023-12-13 03:25:39 +0700

    Config SiteController

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -2,17 +2,6 @@ const Course = require('../models/Course');
 
 const {muntipleMongooseToObject} = require('../../util/mongoose');
 class SiteController {
-  // [GET] /news
-  // index(req, res, next) {
-  //   Course.find({})
-  //     .lean()
-  //     .then((courses) => {
-  //       res.render("home", { courses: muntipleMongooseToObject(courses) });
-  //     })
-  //     .catch((error) => {
-  //       next(error);
-  //     });
-  // }
   index(req, res, next) {
     Course.find({})
       .lean()
